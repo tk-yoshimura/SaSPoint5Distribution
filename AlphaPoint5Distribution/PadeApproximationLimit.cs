5ffe56f31b7f6053ad5bb04131375d8ec9134373
tk-yoshimura <yoshimura8192@gmail.com>
2024-01-28 13:15:44 +0900

    Add: pade approx coef generation

--- AlphaPoint5Distribution/PadeApproximationLimit.cs
+++ AlphaPoint5Distribution/PadeApproximationLimit.cs
@@ -3,26 +3,27 @@ using MultiPrecisionAlgebra;
 using MultiPrecisionCurveFitting;
 
 namespace AlphaPoint5Distribution {
-    internal class PadeApproximation {
-        static void Main() {
+    internal class PadeApproximationLimit {
+        static void Main_() {
             List<(double xmin, double xmax)> ranges = [
-                (0, 1d / 512)
+                (0, 1d / 4)
             ];
 
-            for (double xmin = 1d / 512; xmin < 16; xmin *= 2) {
-                ranges.Add((xmin, xmin * 2));
-            }
-
-            using (StreamWriter sw = new("../../../../results_disused/pade_pdf_precision135.csv")) {
+            using (StreamWriter sw = new("../../../../results_disused/pade_limitpdf_precision135.csv")) {
                 foreach ((double xmin, double xmax) in ranges) {
                     Console.WriteLine($"[{xmin}, {xmax}]");
 
                     List<(MultiPrecision<Pow2.N64> x, MultiPrecision<Pow2.N64> y)> expecteds_range = [];
 
-                    for (double x = xmin, h = (xmax - xmin) / 4096; x <= xmax; x += h) { 
-                        MultiPrecision<Pow2.N16> y = PDFN16.Value(x);
+                    for (MultiPrecision<Pow2.N16> x = xmin, h = (xmax - xmin) / 4096; x <= xmax; x += h) {
+                        if (x != 0) {
+                            MultiPrecision<Pow2.N64> y = PDFN16.Value(1 / (x * x)).Convert<Pow2.N64>() / MultiPrecision<Pow2.N64>.Pow(x.Convert<Pow2.N64>(), 3);
 
-                        expecteds_range.Add((x, y.Convert<Pow2.N64>()));
+                            expecteds_range.Add((x.Convert<Pow2.N64>(), y));
+                        }
+                        else { 
+                            expecteds_range.Add((x.Convert<Pow2.N64>(), 1 / MultiPrecision<Pow2.N64>.Sqrt(8 * MultiPrecision<Pow2.N64>.PI)));
+                        }
                     }
 
                     Console.WriteLine("expecteds computed");
@@ -30,8 +31,8 @@ namespace AlphaPoint5Distribution {
                     MultiPrecision<Pow2.N64> y0 = expecteds_range.Where(item => item.x == xmin).First().y;
                     Vector<Pow2.N64> xs = expecteds_range.Select(item => item.x - xmin).ToArray(), ys = expecteds_range.Select(item => item.y).ToArray();
 
-                    for (int m = 40; m <= 64; m++) {
-                        PadeFitter<Pow2.N64> pade = new(xs, ys, m - 1, m, intercept: y0);
+                    for (int m = 16; m <= 64; m++) {
+                        PadeFitter<Pow2.N64> pade = new(xs, ys, m - 2, m, intercept: y0);
 
                         Vector<Pow2.N64> param = pade.ExecuteFitting();
                         Vector<Pow2.N64> errs = pade.Error(param);
@@ -45,18 +46,18 @@ namespace AlphaPoint5Distribution {
                             max_rateerr = MultiPrecision<Pow2.N64>.Max(MultiPrecision<Pow2.N64>.Abs(errs[i] / ys[i]), max_rateerr);
                         }
 
-                        Console.WriteLine($"m={m-1},n={m}");
+                        Console.WriteLine($"m={m - 2},n={m}");
                         Console.WriteLine($"{max_rateerr:e20}");
 
                         if (max_rateerr < "1e-135" && param.All(item => item.val.Sign == Sign.Plus)) {
                             sw.WriteLine($"x=[{xmin},{xmax}]");
-                            sw.WriteLine($"m={m-1},n={m}");
+                            sw.WriteLine($"m={m - 2},n={m}");
                             sw.WriteLine("numer");
-                            foreach (var (_, val) in param[..(m-1)]) {
+                            foreach (var (_, val) in param[..(m - 2)]) {
                                 sw.WriteLine($"{val:e150}");
                             }
                             sw.WriteLine("denom");
-                            foreach (var (_, val) in param[(m-1)..]) {
+                            foreach (var (_, val) in param[(m - 2)..]) {
                                 sw.WriteLine($"{val:e150}");
                             }
 
